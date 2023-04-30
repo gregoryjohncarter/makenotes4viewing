7c7cf74bdff39d83639ec5a80f9cad492f773289
gregoryjohncarter <gregoryjohncarter@gmail.com>
2023-04-30 14:33:26 -0400

    testing procfile with --chdir

--- server/app.py
+++ server/app.py
@@ -3,16 +3,11 @@ from server.routes import api
 from server.db import init_db
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  app = Flask(__name__, static_folder='client/build',static_url_path='')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
-  CORS(app)
-  app.register_blueprint(api)
-  init_db(app)
-
-  return app
-
-create_app()
+app = Flask(__name__, static_folder='client/build',static_url_path='')
+app.url_map.strict_slashes = False
+app.config.from_mapping(
+  SECRET_KEY='super_secret_key'
+)
+CORS(app)
+app.register_blueprint(api)
+init_db(app)
