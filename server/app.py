c41c2b6b4ce366a3c04075247dc4623e09fd5ba2
gregoryjohncarter <gregoryjohncarter@gmail.com>
2023-04-30 15:39:20 -0400

    testing send from directory serve

--- server/app.py
+++ server/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from server.routes import api
 from server.db import init_db
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../client/build/static', template_folder='../client/build', static_url_path='')
+app = Flask(__name__, static_folder='../client/build', static_url_path='')
 app.url_map.strict_slashes = False
 app.config.from_mapping(
   SECRET_KEY='super_secret_key'
 )
 CORS(app)
 app.register_blueprint(api)
-@app.route('/')
-def root():
-  return render_template('index.html')
+@app.route('/', defaults={'path':''})
+def serve(path):
+  return send_from_directory(app.static_folder,'index.html')
 init_db(app)
