3a8236aef56603671f5425afe05a37def163a0e5
gregoryjohncarter <gregoryjohncarter@gmail.com>
2023-04-30 14:45:16 -0400

    changed static folder path with rel

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from server.routes import api
 from server.db import init_db
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='client/build',static_url_path='')
+app = Flask(__name__, static_folder='../client/build',static_url_path='')
 app.url_map.strict_slashes = False
 app.config.from_mapping(
   SECRET_KEY='super_secret_key'
