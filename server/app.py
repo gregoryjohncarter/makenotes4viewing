0daba0453bee5e3962b474d378c024198d2a0f90
gregoryjohncarter <gregoryjohncarter@gmail.com>
2023-04-30 15:08:03 -0400

    testing render template / template folder

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 from server.routes import api
 from server.db import init_db
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../client/build',static_url_path='')
+app = Flask(__name__, static_folder='../client/build', template_folder='../client/build', static_url_path='')
 app.url_map.strict_slashes = False
 app.config.from_mapping(
   SECRET_KEY='super_secret_key'
@@ -12,7 +12,7 @@ CORS(app)
 app.register_blueprint(api)
 @app.route('/', defaults={'path': ''})
 def serve(path):
-  return send_from_directory(app.static_folder, 'index.html')
+  return render_template('index.html')
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
 init_db(app)
